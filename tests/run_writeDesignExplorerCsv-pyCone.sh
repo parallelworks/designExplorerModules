df35a261b5f9fed0b75020294605acbec69edaf0
marmar-m <marmar@parallelworks.com>
2017-10-09 17:09:30 -0500

    Update mexdex to the latest version and test

--- tests/run_writeDesignExplorerCsv-pyCone.sh
+++ tests/run_writeDesignExplorerCsv-pyCone.sh
@@ -3,4 +3,5 @@
 
 python      ../mexdex/writeDesignExplorerCsv.py \
 	--imagesDirectory "../example_inputs/pyCone/results/case_{:d}" \
+    --casesList_paramValueDelimiter  ":"   \
 	../example_inputs/pyCone/cases.list ../example_inputs/pyCone/kpi.json .  ../example_outputs/pyCone/output.csv 
